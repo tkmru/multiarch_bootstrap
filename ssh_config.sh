dedc092fae93059e252f55b10d06b7ebe9c2aec8
tkmru <i.am.tkmru@gmail.com>
2017-07-10 20:45:08 +0900

    add ssh_config.sh

---
+++ ssh_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo '
+Host mipsle
+  HostName localhost
+  User root
+  Port 2222
+
+Host mipsbe
+  HostName localhost
+  User root
+  Port 2223
+
+Host armle
+  HostName localhost
+  User root
+  Port 2224
+
+Host aarch64
+  HostName localhost
+  User root
+  Port 2225
+' >> ~/.ssh/config
